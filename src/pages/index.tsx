b88b422a8450acddd06b62e1ed62635f2932d29f
RenanC <renancostam@gmail.com>
2020-09-07 17:08:28 -0300

    Fix import error of Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from "react"
 import { PageProps, Link } from "gatsby"
 
-import Layout from "../components/Header"
+import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
 const IndexPage: React.FC<PageProps> = ({ data, path }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Gatsby supports TypeScript by default!</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit voluptatem
+      doloremque, iste necessitatibus officia tempore magni nam provident,
+      repellendus facilis nesciunt nostrum blanditiis, nisi ipsam! Cumque
+      commodi consequatur ullam cum.
+    </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
